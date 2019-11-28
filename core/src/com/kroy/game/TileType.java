4794f0968acab5535aa69aa903755a71f830c1c4
fy677 <fy677@york.ac.uk>
2019-11-28 19:34:33 +0000

    Rearanged enum
    changed the orderr so that it alligned with the texture codes for the map

--- core/src/com/kroy/game/TileType.java
+++ core/src/com/kroy/game/TileType.java
@@ -2,10 +2,11 @@ package com.kroy.game;
 
 public enum TileType {
 
-    TILE_TYPES_GRASS,
     TILE_TYPES_ROAD,
-    TILE_TYPES_STATION,
+    TILE_TYPES_WATER,
+    TILE_TYPES_GRASS,
+    TILE_TYPES_BUILDING,
     TILE_TYPES_FORTRESS,
-    TILE_TYPES_WATER;
+    TILE_TYPES_STATION;
 
 }
