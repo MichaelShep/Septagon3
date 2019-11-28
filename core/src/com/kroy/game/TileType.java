fedee9d5e16b01e12e4ce2d26f3ceb9be15d3898
fy677 <fy677@york.ac.uk>
2019-11-28 19:35:19 +0000

    Revert "Rearanged enum"
    This reverts commit 4794f0968acab5535aa69aa903755a71f830c1c4.

--- core/src/com/kroy/game/TileType.java
+++ core/src/com/kroy/game/TileType.java
@@ -2,11 +2,10 @@ package com.kroy.game;
 
 public enum TileType {
 
-    TILE_TYPES_ROAD,
-    TILE_TYPES_WATER,
     TILE_TYPES_GRASS,
-    TILE_TYPES_BUILDING,
+    TILE_TYPES_ROAD,
+    TILE_TYPES_STATION,
     TILE_TYPES_FORTRESS,
-    TILE_TYPES_STATION;
+    TILE_TYPES_WATER;
 
 }
