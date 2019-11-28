9dd7247ab3af77beff178cd627be6b0e5dcbece2
fy677 <fy677@york.ac.uk>
2019-11-28 19:36:51 +0000

    Revert "Revert "Rearanged enum""
    This reverts commit fedee9d5e16b01e12e4ce2d26f3ceb9be15d3898.

--- core/src/com/kroy/game/Map.java
+++ core/src/com/kroy/game/Map.java
@@ -1,9 +1,8 @@
 package com.kroy.game;
 
 import java.io.*;
-
-
-
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Map {
@@ -15,155 +14,120 @@ public class Map {
     private int mapWidth,mapHeight;
     private int shiftX,shiftY;
 
-
-
-
-    Map()
+    public Map(String fileName)
     {
-        //default map
-        mapWidth = Constants.getMapWidth();
-        mapHeight = Constants.getMapHeight();
+        ArrayList<String[]> mapTileData = new ArrayList<String[]>();
+        String directory = System.getProperty("user.dir") + "\\core\\src\\Data\\" + fileName;
+
+        try
+        {
+            mapTileData = readMapCSV(directory);
+        }
+        catch (IOException e)
+        {
+            System.out.println(e + "Map Could not be read" );
+            System.exit(0);
+        }
+
+        mapWidth = mapTileData.get(0).length;
+        mapHeight = mapTileData.size();
         mapData = new Tile[mapWidth][mapHeight];
 
         shiftX = (Constants.getResolutionWidth() - (mapWidth*Constants.getTileSize()))/2;
         shiftY = (Constants.getResolutionHeight() - (mapHeight*Constants.getTileSize()))/2;
 
-        stationPosition = null;
-        fortressPositions = new Tile[Constants.getFortressCount()];
-
-        try
-        {
-            generateMap();
-        }
-        catch (IOException e)
-        {
-            System.out.println(e + "Map Could not be generated" );
-            System.exit(0);
-        }
+        generateMap(mapTileData);
     }
 
 
+    ArrayList<String[]> readMapCSV(String mapCSVFile) throws IOException
+    {
+        ArrayList<String[]> rowData = new ArrayList<String[]>();
+        String row;
+        rowData = null;
 
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader(mapCSVFile));
 
-
-    public void generateMap() throws IOException {
-
-        String directory = System.getProperty("user.dir") + "\\core\\src\\Data\\" + Constants.getMapFileName();
-
-        BufferedReader csvReader;
-
-        String texture;
-
-        int maxFortresses = fortressPositions.length;
-        int fortressPlaced = 0;
-
-        String rowData;
-        int rowCount = Constants.getMapHeight()-1;
-        int cellData = 0;
-
-        try
-        {
-
-            csvReader = new BufferedReader(new FileReader(directory));
-            System.out.println("File Loaded");
-            while ((rowData = csvReader.readLine()) != null) {
-                String[] data = rowData.split(",");
-                // do something with the data
-                for (int cell = 0; cell < data.length; cell++)
-                {
-                    Boolean placed = false;
-                    cellData = Integer.parseInt(data[cell]);
-                    texture = mapTextures(cellData);
-
-                    if (texture == "fortressTile.png")
-                    {
-                        if (fortressPlaced >= maxFortresses)
-                        {
-                            texture = "grassTile.png";
-                        }
-                        else
-                        {
-                            mapData[cell][rowCount] = new Tile(cell,rowCount, texture, TileType.values()[cellData]);
-                            for(int i=0; i<maxFortresses;i++)
-                            {
-                                if (fortressPositions[i] == null)
-                                {
-                                    fortressPositions[i] = mapData[cell][rowCount];
-                                    break;
-                                }
-                            }
-
-                            fortressPlaced++;
-                            placed = true;
-                        }
-
-                    }
-                    else if (texture == "stationTile.png")
-                    {
-                        if (stationPosition == null)
-                        {
-                            mapData[cell][rowCount] = new Station(cell,rowCount);
-                            placed = true;
-                        }
-                    }
-
-                    if (!(placed))
-                    {
-                        mapData[cell][rowCount] = new Tile(cell,rowCount, texture, TileType.values()[cellData]);
-                    }
-
-
-                }
-
-                rowCount--;
-            }
-            csvReader.close();
-
-            if (fortressPlaced < maxFortresses)
+            while ((row = csvReader.readLine()) != null)
             {
-                throw new IllegalArgumentException("Input Map does not contain enough fortresses as specified in Constants.Java");
+                String[] data = row.split(",");
+                rowData.add(data);
             }
 
-
-
+            csvReader.close();
         }
         catch(FileNotFoundException e)
         {
-            throw new FileNotFoundException(Constants.getMapFileName()+"\n");
+            throw new FileNotFoundException();
         }
         catch (IOException e)
         {
             throw new IOException();
         }
 
+        return rowData;
+    }
 
 
+    public void generateMap(ArrayList<String[]> mapTileData) {
 
+        int tileCode;
 
-
+        for (int width = 0; width < mapWidth; width++)
+        {
+            for (int height = 0; height < mapHeight; height++)
+            {
+                tileCode = Integer.parseInt(mapTileData.get(width)[height]);
+                switch (tileCode)
+                {
+                    case 1:
+                        mapData[width][height] = new Tile(width, height,mapTextures(tileCode), TileType.values()[tileCode]);
+                    case 2:
+                        mapData[width][height] = new Tile(width, height,mapTextures(tileCode), TileType.values()[tileCode]);
+                    case 3:
+                        mapData[width][height] = new Tile(width, height,mapTextures(tileCode), TileType.values()[tileCode]);
+                    case 4:
+                        mapData[width][height] = new Tile(width, height,mapTextures(tileCode), TileType.values()[tileCode]);
+                    case 5:
+                        mapData[width][height] = new Tile(width, height,mapTextures(tileCode), TileType.values()[tileCode]);
+                    case 6:
+                        mapData[width][height] = new Tile(width, height,mapTextures(tileCode), TileType.values()[tileCode]);
+                }
+            }
+        }
     }
 
 
     private String mapTextures(int textureCode)
     {
         switch (textureCode) {
-            case 0: {
-                return "grassTile.png";
-            }
-            case 1: {
+            case 1:
                 return "roadTile.png";
-            }
-            case 2: {
-                return "stationTile.png";
-            }
-            case 3:{
+            case 2:
+                return "waterTile.png";
+            case 3:
+                return rndTexture("GreeneryTexture");
+            case 4:
+                return rndTexture("BuildingTexture");
+            case 5:
                 return "fortressTile.png";
-            }
-
+            case 6:
+                return "stationTile.png";
+            default:
+                throw new IllegalArgumentException(textureCode + " is not implemented in mapTextures()");
         }
+    }
 
-        throw new IllegalArgumentException(textureCode + " is not implemented in mapTextures()");
 
+    String rndTexture(String textureType)
+    {
+        Random random = new Random();
+        String path = System.getProperty("user.dir") + "\\core\\src\\assets\\" + textureType;
+        File folder = new File(path);
+        File[] textures = folder.listFiles();
+
+        return textures[random.nextInt(textures.length)].getName();
     }
 
 
