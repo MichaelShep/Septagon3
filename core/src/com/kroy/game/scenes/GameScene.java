8b002000f1f5083a20b52c4263c045fc420422ce
thanhnguyenhu66 <thanh.nguyenhuu66@gmail.com>
2020-02-14 17:20:36 +0000

    bullets working

--- core/src/com/kroy/game/scenes/GameScene.java
+++ core/src/com/kroy/game/scenes/GameScene.java
@@ -5,7 +5,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kroy.game.*;
 import com.kroy.game.Character;
 import com.kroy.game.rendering.Renderer;
