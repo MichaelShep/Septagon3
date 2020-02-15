fd43777817e6374abc6c8391c28d10af941a6055
samhe <samueloharrison@gmail,com>
2020-02-15 12:40:08 +0000

    Basic Human Tests

--- tests/src/com/kroy/tests/HumanTests.java
+++ tests/src/com/kroy/tests/HumanTests.java
@@ -12,15 +12,38 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import static org.junit.Assert.*;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @RunWith(GdxTestRunner.class)
 public class HumanTests {
+    Human testHuman;
+
+    @Before
+    public void init(){
+        testHuman = new Human (false, 3);
+    }
 
     @Test
     public void testConstructor(){
-        Human testHuman = new Human(false, 3);
+        testHuman = new Human(false, 3);
+        assertFalse(testHuman.isMyTurn());
+        assertEquals(3, testHuman.getAliveCharacters());
+        testHuman = new Human(true, 1);
+        assertTrue(testHuman.isMyTurn());
+        assertEquals(1, testHuman.getAliveCharacters());
+    }
+
+    @Test
+    public void testCreateFireEngine(){
+        FireEngine testEngine = testHuman.createFireEngine(0);
+        assertEquals(16, testEngine.getMaxHealth());
+        assertEquals(5, testEngine.getDamage());
+        assertEquals(5, testEngine.getRange());
+        assertEquals(4, testEngine.getSpeed());
+        assertEquals(4, testEngine.getWaterCapacity());
+
     }
 }
